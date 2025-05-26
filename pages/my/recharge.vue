5ce3ca0a4a45ddb868afde6556d7d0b8b8beb290
晓 <457294316@qq.com>
2025-05-26 15:35:50 +0800

    fix

--- pages/my/recharge.vue
+++ pages/my/recharge.vue
@@ -30,21 +30,20 @@
 			</view>
 
 			<view style="margin-top: 52rpx;font-weight: bold;font-size: 32rpx;">充值明细</view>
-			<view style="margin-top: 30rpx;padding: 34rpx;box-shadow: 0rpx 6rpx 32rpx 2rpx rgba(0,0,0,0.08);border-radius: 24rpx;">
-				<view v-for="(item, index) in list" :key="index" @click="goDetail(item.id)">
-					<view style="display: flex;justify-content: space-between;">
-						<view style="font-weight: bold;font-size: 30rpx;">
-							<view v-if="item.status == 1">充值待付款</view>
-							<view v-else-if="item.status == 2">充值已付款</view>
-							<view v-else-if="item.status == 3">充值已到账</view>
-							<view v-else-if="item.status == 4">充值失败</view>
-						</view>
-						<view style="font-weight: bold;font-size: 30rpx;">{{ item.money }} USDT</view>
+			<view v-for="(item, index) in list" :key="index" @click="goDetail(item.id)"
+				style="margin-top: 30rpx;padding: 34rpx;box-shadow: 0rpx 6rpx 32rpx 2rpx rgba(0,0,0,0.08);border-radius: 24rpx;">
+				<view style="display: flex;justify-content: space-between;">
+					<view style="font-weight: bold;font-size: 30rpx;">
+						<view v-if="item.status == 1">充值待付款</view>
+						<view v-else-if="item.status == 2">充值已付款</view>
+						<view v-else-if="item.status == 3">充值已到账</view>
+						<view v-else-if="item.status == 4">充值失败</view>
 					</view>
-					<view style="margin-top: 28rpx;display: flex;justify-content: space-between;">
-						<view style="font-weight: 400;font-size: 24rpx;color: #A9ABB6;">{{ item.time }}</view>
-						<!-- <view style="font-weight: 400;font-size: 24rpx;color: #377FF3;">倒计时 15:00</view> -->
-					</view>
+					<view style="font-weight: bold;font-size: 30rpx;">{{ item.money }} USDT</view>
+				</view>
+				<view style="margin-top: 28rpx;display: flex;justify-content: space-between;">
+					<view style="font-weight: 400;font-size: 24rpx;color: #A9ABB6;">{{ item.time }}</view>
+					<!-- <view style="font-weight: 400;font-size: 24rpx;color: #377FF3;">倒计时 15:00</view> -->
 				</view>
 			</view>
 		</view>
